7c79bf69fddb14d82896c74b42a81330a167771d
Vivekananda Dayananda <vivekananda.dayananda@amd.com>
2023-05-02 13:36:27 -0700

    interface:iio_driver Implement channel and device attr
    implements the following features
    read channel attribute
    write channel attribute
    read device attribute
    write device attribute
    constructor to populate device channel vector

--- lib/src/interface/iio_drv.h
+++ lib/src/interface/iio_drv.h
@@ -8,17 +8,23 @@
 
 #include <string>
 #include <iio.h>
+#include <vector>
 
-class IIO_Driver {
+class IIO_Driver
+{
 public:
-	IIO_Driver(const std::string& deviceName);
+	IIO_Driver(const std::string &deviceName);
 	~IIO_Driver();
-	int readChannel(const std::string channelName);
-	void writeChannel(const std::string channelName, int value);
+	int readChannel(const unsigned int index, const std::string &attrName);
+	int writeChannel(const unsigned int index, const std::string &attrName, const std::string &value);
+	int readDeviceattr(const std::string &attrName);
+	int writeDeviceattr(const std::string &attrName, const std::string &value);
+
 private:
 	struct iio_context *ctx;
 	struct iio_device *dev;
+	unsigned int channelCount;
+	std::vector<iio_channel *> channels;
 };
-
 
 #endif //_IIO_DRV_H_
