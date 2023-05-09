ddecc1db281749790c978791d4d2ecbca2fc42c1
Vivekananda Dayananda <vivekananda.dayananda@amd.com>
2023-05-09 11:25:31 -0700

    lib/svpwm: Implementation of Svpwm interface

--- lib/src/svpwm.cpp
+++ lib/src/svpwm.cpp
@@ -2,3 +2,39 @@
  * Copyright (C) 2023 Advanced Micro Devices, Inc.
  * SPDX-License-Identifier: MIT
  */
+
+#include "svpwm.h"
+#define SCALE 65536
+
+const std::string Svpwm::kSvpwmDriverName = "hls_svpwm_duty";
+
+Svpwm::Svpwm()
+{
+    m_Svpwm_IIO_Handle = new IIO_Driver(kSvpwmDriverName);
+}
+
+int Svpwm::setSampleII(int sample)
+{
+    return m_Svpwm_IIO_Handle->writeDeviceattr("sample_ii", std::to_string(sample).c_str());
+}
+
+int Svpwm::setDcLink(int volt)
+{
+    volt *= SCALE;
+    return m_Svpwm_IIO_Handle->writeDeviceattr("dc_link_ref_voltage", std::to_string(volt).c_str());
+}
+
+int Svpwm::setMode(int mode)
+{
+    return m_Svpwm_IIO_Handle->writeDeviceattr("dc_src_mode", std::to_string(mode).c_str());
+}
+
+int Svpwm::startSvpwm()
+{
+    return m_Svpwm_IIO_Handle->writeDeviceattr("ap_ctrl", "1");
+}
+
+Svpwm::~Svpwm()
+{
+    delete m_Svpwm_IIO_Handle;
+}
---
+++ lib/src/svpwm.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2023 Advanced Micro Devices, Inc.
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef _SVPWM_H_
+#define _SVPWM_H_
+
+#include "interface/iio_drv.h"
+#include <string>
+
+class Svpwm
+{
+
+public:
+        Svpwm();
+        int setSampleII(int sample);
+        int setDcLink(int volt);
+        int setMode(int mode);
+        int startSvpwm();
+        ~Svpwm();
+
+private:
+        IIO_Driver *m_Svpwm_IIO_Handle;
+        static const std::string kSvpwmDriverName;
+};
+
+#endif /* _SVPWM_H_ */
