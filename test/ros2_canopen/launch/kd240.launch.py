492d5d97fc2ead4179842c312aa559acba3c8aad
Pallav Joshi <pallav.joshi@amd.com>
2024-05-13 14:33:04 -0700

    Add config files for ros2_canopen
    Signed-off-by: Pallav Joshi <pallav.joshi@amd.com>

---
+++ test/ros2_canopen/launch/kd240.launch.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+import os
+import sys
+
+import launch
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    slave_eds_path = os.path.join(
+        get_package_share_directory("foc_motor"), "config", "motor_control", "foc-mc.eds"
+    )
+    slave_node_1 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("canopen_fake_slaves"), "launch"),
+                "/cia402_slave.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "node_id": "4",
+            "node_name": "kd240",
+            "slave_config": slave_eds_path,
+        }.items(),
+    )
+    master_bin_path = os.path.join(
+        get_package_share_directory("foc_motor"),
+        "config",
+        "motor_control",
+        "master.bin",
+    )
+    if not os.path.exists(master_bin_path):
+        master_bin_path = ""
+
+    device_container = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("canopen_core"), "launch"),
+                "/canopen.launch.py",
+            ]
+        ),
+        launch_arguments={
+            "master_config": os.path.join(
+                get_package_share_directory("foc_motor"),
+                "config",
+                "motor_control",
+                "master.dcf",
+            ),
+            "master_bin": master_bin_path,
+            "bus_config": os.path.join(
+                get_package_share_directory("foc_motor"),
+                "config",
+                "motor_control",
+                "bus.yml",
+            ),
+            "can_interface_name": "vcan0",
+        }.items(),
+    )
+
+    ld.add_action(device_container)
+    ld.add_action(slave_node_1)
+
+    return ld
