ab03a0efe45a3142a1c83ecb994c56a2016b7e51
Pallav Joshi <pallav.joshi@amd.com>
2024-06-19 04:17:57 -0700

    test: Add ros2 control pakage for testing
    Signed-off-by: Pallav Joshi <pallav.joshi@amd.com>

---
+++ test/ros2_co_control/launch/kd240_control.launch.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+import os
+import sys
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("ros2_co_control"),
+                    "urdf",
+                    "kd240.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+    robot_control_config = PathJoinSubstitution(
+        [FindPackageShare("ros2_co_control"), "config", "ros2_controllers.yaml"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_control_config],
+        output="screen",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    forward_position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_velocity_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    nodes_to_start = [
+        control_node,
+        joint_state_broadcaster_spawner,
+        forward_position_controller_spawner,
+        robot_state_publisher_node,
+    ]
+
+    return LaunchDescription(nodes_to_start)
